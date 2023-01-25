70c66d9da557df83059d730f5723261e028cd0cf
Santiago Burke <santiburke04@gmail.com>
2023-01-25 18:51:10 -0300

    Reentrega

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,47 +1,70 @@
 import { Router } from 'express'
-import { ProductManager } from '../index'
+import ProductManager from '../src/index.js'
+import { upload } from '../middlewares/multer.js'
+
 const productRouter = Router()
+const productManager = new ProductManager('./archivos/productos.json') 
 
-const productManager = new ProductManager('../archivos/productos.json')
-
-productRouter.get('/', async (req, res) => {
-    const products = await productManager.getProducts()
-    const { limit } = req.query
-    if (limit) {
-        res.json(products.slice(0, limit))
-    } else {
-        res.json(products)
+productRouter.get('/',async(req,res)=>{
+    try {
+        const {limit} = req.query
+        const products = await productManager.getProducts(limit || 'max')
+        console.log(products)
+        res.json({products})
+    } catch (error) {
+        res.send(error)
     }
+    
 })
 
-productRouter.get('/:pid', async (req, res) => {
-    const products = await productManager.getProducts()
-    const { pid } = req.params
-    const productId = products.find(p => p.id === parseInt(pid))
-    if (pid) {
-        res.json(productId)
-    } else {
-        res.send('No existe ningun producto con ese ID')
+productRouter.get('/:idProduct',async(req,res)=>{
+    const {idProduct} = req.params
+    console.log(idProduct)
+    try {
+        const product = await productManager.getProductById(idProduct)
+        console.log(product)
+        if(product){
+            res.json({product})
+    }else{
+        res.send('Producto no encontrado')
     }
+} catch (error) {
+    res.send(error)
+}
 })
 
-productRouter.post('/', async (req, res) => {
+productRouter.post('/', upload.single('file'), async(req, res) => {
     const product = req.body
-    const products = await productManager.addProducts(product)
-    res.status(200).json({message: 'Usuario creado', products})
+    console.log(product)
+    const addNewProduct = await productManager.addProduct(product)
+    console.log(addNewProduct)
+    res.json({ message: 'Producto agregado con exito', addNewProduct })
 })
 
-productRouter.put('/', async(req,res)=>{
-    const {id} = req.params
+productRouter.put('/:idProduct', async(req, res) => {
+    const {idProduct} = req.params
     const product = req.body
-    const products = await productManager.updateProduct(parseInt(id), product)
-    res.status(200).json({message: 'Usuario modificado', products})
+    try {
+        const updateProduct = await productManager.updateProduct(idProduct, Object.assign({},product))
+        console.log(updateProduct)
+        res.json({ message: 'Producto modificado con exito'})
+    } catch (error) {
+        console.log('Error al modificar el producto')
+        return error
+    }
+    
 })
 
-productRouter.delete('/', async(req,res)=>{
-    const {id} = req.params
-    const products = await productManager.deleteProduct(parseInt(id))
-    res.status(200).json({message: 'Usuario eliminado', products})
+productRouter.delete('/:idProduct', async(req, res) => {
+    const {idProduct} = req.params
+    try {
+        const deleteProduct = await productManager.deleteProduct(idProduct)
+        console.log(deleteProduct)
+        res.json({ message: 'Producto eliminado con exito'})
+    } catch (error) {
+        console.log('error')
+        return error
+    }
 })
 
 export default productRouter
--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
-import { productRouter } from '../routes/products.router.js'
-import { cartRouter } from '../routes/carts.router.js'
+import productsRouter from '../routes/products.router.js'
+import cartRouter from '../routes/carts.router.js'
 
-const PORT = 8080
 const app = express()
+const PORT = 8080
 
-app.use('/api/products', productRouter)
-app.use('/api/carts', cartRouter)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api/products', productsRouter)
+app.use('/api/cart', cartRouter)
+
+app.get('/',(req,res)=>{
+    res.send('Ruta raiz')
+})
 
 app.listen(PORT, () => {
-    console.log(`Escuchando al puerto: ${PORT}`)
+  console.log(`Escuchando al puerto ${PORT}`)
 })
